cda715c86d6a179835ddcfba0b3917ddeb580f62
gabrielholz <gabrielgustavogramsholz@gmail.com>
2022-06-25 18:49:53 -0300

    Checkpoint finalizado.

--- avaliacao_final/index.js
+++ avaliacao_final/index.js
@@ -1,17 +1,113 @@
-// Passo 1 (1,5 pontos)
-// Crie uma função construtora que tenha como atributos: nome (string), quantidade de faltas (number) e notas (array de números). 
+const fs = require('fs');
 
-//  Passo 2 (1,5 pontos)
-// Na função construtora crie o método calcularMedia que retorna a média de suas notas. Também terá um método chamado faltas, que simplesmente aumenta o número de faltas em 1. Crie alguns alunos para testar a sua função construtora. 
+const alunosDados = fs.readFileSync('./alunos.csv', 'utf8');
 
-//  Passo 3 (1,5 pontos)
-// crie o objeto literal curso que tem como atributos: nome do curso (string), nota de aprovação (number), faltas máximas (number) e uma lista de estudantes (um array composto pelos alunos criados no passo 2).
 
-//  Passo 4 (1,5 pontos)
-// 	Crie o método que permite adicionar alunos à lista do curso, ou seja, quando chamamos nosso método em nosso objeto curso, deverá adicionar um aluno a mais na propriedade lista de estudantes do objeto curso.
+function AlunoConstruct(nome, qtd_faltas, notas) {
+  return {
+    nome: nome,
+    qtd_faltas: qtd_faltas,
+    notas: notas,
+    calcularMedia() {
+      let somaNotas = 0;
+      for (i in this.notas) {
+        somaNotas += this.notas[i]
+      }
+      return (somaNotas / this.notas.length).toFixed(2);
 
-//  Passo 5 (2 pontos)
-// 	Crie um método para o objeto curso que receba um aluno (como parâmetro) e retorne true se ele aprovou no curso ou false em caso de reprovação. Para ser aprovado, o aluno tem que ter uma média igual ou acima da nota de aprovação  e ter menos faltas que faltas máximas. Se tiver a mesma quantidade, tem que estar 10% acima da nota de aprovação.
+    },
+    faltas() {
+      this.qtd_faltas++
+      return this.qtd_faltas;
+    }
 
-//  Passo 6 (2 pontos)
-// 	Crie um método para o objeto curso que percorra a lista de estudantes e retorne um array de booleanos com os resultados se os alunos aprovaram ou não. 
+  };
+}
+
+
+let listaAlunos = []
+
+let linhas = alunosDados.split("\r\n");
+for (let i = 1; i < linhas.length; i++) {
+  const linha = linhas[i];
+  let dados = linha.split(";");
+  let notasString = dados[2].replace('[', '').replace(']', '').split(",");
+  let notasNumber = []
+  for (j in notasString) {
+    notasNumber.push(parseFloat(notasString[j]))
+  }
+  const aluno = dados[0];
+  const qtd_faltas = dados[1];
+
+  let novoAluno = new AlunoConstruct(aluno, qtd_faltas, notasNumber)
+  listaAlunos.push(novoAluno);
+
+
+}
+
+let curso = {
+
+  nomeDoCurso: 'Certified Tech Developer',
+  notaDeAprovacao: 7.5,//+10% = 8.25
+  faltasMaximas: 3,
+  listaDeEstudantes: listaAlunos,
+
+  cadastrarAluno(aluno, qtd_faltas, notas) {
+    let novoAluno = new AlunoConstruct(aluno, qtd_faltas, notas)
+    this.listaDeEstudantes.push(novoAluno);
+  },
+
+  verificarAprovacao(aluno) {
+    for (let i in this.listaDeEstudantes) {
+      const estudante = this.listaDeEstudantes[i];
+      if (estudante.nome === aluno) {
+
+        const notaMediaAluno = estudante.calcularMedia()
+
+        if (notaMediaAluno >= this.notaDeAprovacao) {
+          if (estudante.qtd_faltas < this.faltasMaximas) {
+            return true;
+          } else if (estudante.qtd_faltas == this.faltasMaximas) {
+            if (notaMediaAluno >= (this.notaDeAprovacao * 1.1)) return true;
+            else return false;
+          }
+        }
+        else return false;
+      }
+    }
+
+  },
+
+  verificarAprovacaoEmMassa() {
+    for (i in this.listaDeEstudantes) {
+      const estudante = this.listaDeEstudantes[i]
+      const notaMedia = this.listaDeEstudantes[i].calcularMedia()
+      if (this.verificarAprovacao(estudante.nome)) console.log(`
+    
+      | Estudante: ${estudante.nome}
+      | Notas: ${notaMedia} 
+      | Faltas: ${estudante.qtd_faltas} 
+      | Situação: Aprovado
+      _____________________________________`);
+      else console.log(`
+      | Estudante: ${estudante.nome} 
+      | Notas: ${notaMedia}  
+      | Faltas: ${estudante.qtd_faltas}  
+      | Situação: Reprovado
+      -------------------------------------`);
+    }
+  }
+
+
+}
+
+
+
+console.log(listaAlunos[0])
+console.log(`Adicionando falta... Faltas aluno ${listaAlunos[0].nome}:  ${listaAlunos[0].faltas()}`)
+console.log(`Calculando media... Media aluno ${listaAlunos[0].nome}: ${listaAlunos[0].calcularMedia()}`)
+curso.cadastrarAluno('Gabriela', 1, [8, 10, 8.5, 9, 7])
+curso.verificarAprovacaoEmMassa()
+
+
+
